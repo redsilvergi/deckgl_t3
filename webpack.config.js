aa4ccc99893c94ded7c5c33f707afc465753d05f
silver <eungi429@gmail.com>
2023-05-22 16:36:46 +0900

    deploy test 4

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,16 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  resolve: {
-    fallback: {
-      path: require.resolve("path-browserify"),
-      os: require.resolve("os-browserify/browser"),
-    },
-  },
   plugins: [
     new HTMLWebpackPlugin({
       template: path.resolve(__dirname, "./src", "index.html"),
     }),
   ],
-  output: { path: path.resolve(__dirname, "./dist"), filename: "main.js" },
-  entry: { index: path.resolve(__dirname, "./src", "index.js") },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js",
+  },
+  entry: {
+    index: path.resolve(__dirname, "./src", "index.js"),
+  },
 };
